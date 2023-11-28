dc6974d2da4a8c4e49b3c5f54edf20259a7c0375
Chris Green <greenc@fnal.gov>
2023-11-28 17:10:08 -0600

    [py-sphinx-design] New version, updated dependencies

--- var/spack/repos/builtin/packages/py-sphinx-design/package.py
+++ var/spack/repos/builtin/packages/py-sphinx-design/package.py
@@ -12,17 +12,21 @@ class PySphinxDesign(PythonPackage):
     homepage = "https://sphinx-design.readthedocs.io"
     pypi = "sphinx-design/sphinx_design-0.3.0.tar.gz"
 
-    maintainers("ax3l", "adamjstewart")
+    maintainers("ax3l", "adamjstewart", "greenc-FNAL")
 
+    version("0.5.0", sha256="e8e513acea6f92d15c6de3b34e954458f245b8e761b45b63950f65373352ab00")
     version("0.4.1", sha256="5b6418ba4a2dc3d83592ea0ff61a52a891fe72195a4c3a18b2fa1c7668ce4708")
     version("0.4.0", sha256="b92948614900967499617d99aadd38ce5975ede924a18c7478cc6b8ec188f76b")
     version("0.3.0", sha256="7183fa1fae55b37ef01bda5125a21ee841f5bbcbf59a35382be598180c4cefba")
 
-    depends_on("python@3.7:", type=("build", "run"))
+    depends_on("python@3.7:", when="@:0.4", type=("build", "run"))
+    depends_on("python@3.8:", when="@0.5:", type=("build", "run"))
     depends_on("py-flit-core@3.4:3", type=("build"))
     depends_on("py-sphinx@4:5", when="@0.3", type=("build", "run"))
-    depends_on("py-sphinx@4:6", when="@0.4:", type=("build", "run"))
+    depends_on("py-sphinx@4:6", when="@0.4", type=("build", "run"))
+    depends_on("py-sphinx@5:7", when="@0.5:", type=("build", "run"))
     depends_on("py-pytest@7.1:", type="test")
     depends_on("py-pytest-cov", type="test")
     depends_on("py-pytest-regressions", type="test")
-    depends_on("py-myst-parser@0.18.0:", type="test")
+    depends_on("py-myst-parser@0.18.0:1", when="@:0.4", type="test")
+    depends_on("py-myst-parser@1:2", when="@0.5:", type="test")
