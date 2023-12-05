83e01bed722fca38a0e0d54b32636cf4f9eb4644
Chris Green <greenc@fnal.gov>
2023-12-05 17:11:33 -0600

    Style fix

--- var/spack/repos/builtin/packages/perl-carp-assert/package.py
+++ var/spack/repos/builtin/packages/perl-carp-assert/package.py
@@ -15,10 +15,11 @@ class PerlCarpAssert(PerlPackage):
     maintainers("chissg", "gartung", "marcmengel", "vitodb")  # AUTO-CPAN2Spack
 
     version("0.22", sha256="807ea97c6bed76ac2e4969efba7dae48fefeb9f28797f112671b3ac8a49355f7")
-    version("0.21",
-            url="https://cpan.metacpan.org/authors/id/N/NE/NEILB/Carp-Assert-0.21.tar.gz",
-            sha256="924f8e2b4e3cb3d8b26246b5f9c07cdaa4b8800cef345fa0811d72930d73a54e",
-            )
+    version(
+        "0.21",
+        url="https://cpan.metacpan.org/authors/id/N/NE/NEILB/Carp-Assert-0.21.tar.gz",
+        sha256="924f8e2b4e3cb3d8b26246b5f9c07cdaa4b8800cef345fa0811d72930d73a54e",
+    )
 
     depends_on("perl@5.6:", type="run")
 
